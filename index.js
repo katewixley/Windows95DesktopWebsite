5328d0d3fe3147c65782c23d8a9730254291b03e
Jeremy Southon <southon55@gmail.com>
2018-09-07 16:47:49 +1200

    Added clicking sound, and draggable windows

--- index.js
+++ index.js
@@ -10,8 +10,24 @@ $(document).ready(function () {
         }
     })
 
+
     $('.close').click(function () {
-       $('.close').parent().css("display","none")
+        $('.close').parent().css("display", "none")
     })
+
+    $(function () {
+        $("#ErrorMessage").draggable();
+    });
+
 });
+
+window.addEventListener('keydown', function (e) {
+    const audio = document.querySelector(`audio`);
+    audio.play();
+})
+
+window.addEventListener('click', function () {
+    const audio = document.querySelector(`audio[data-key="60"]`);
+    audio.play();
+})
 
